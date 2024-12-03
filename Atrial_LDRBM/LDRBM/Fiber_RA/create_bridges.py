4ab441aa6f509220235d2d1b0a14a43a738aa07a
Pascal Maierhofer <pascal.maierhofer@student.kit.edu>
2024-12-03 16:36:41 +0100

    Changed bridge merging to difference

--- Atrial_LDRBM/LDRBM/Fiber_RA/create_bridges.py
+++ Atrial_LDRBM/LDRBM/Fiber_RA/create_bridges.py
@@ -304,9 +304,9 @@ def add_free_bridge(args, la_epi, ra_epi, CS_p, df, job):
         mesh_D = pymesh.load_mesh(job.ID + "/bridges/" + str(var) + "_bridge_resampled.obj")
         mesh_E = pymesh.load_mesh(job.ID + "/bridges/" + str(var) + "_earth.obj")
         # # Warning: set -1 if pts normals are pointing outside
-        output_mesh_2 = pymesh.boolean(mesh_D, mesh_E, operation="union", engine="corefinement")
-            # Use difference if the endo normals are pointing inside
-        #    output_mesh_2 = pymesh.boolean(mesh_E, mesh_D, operation="difference", engine="corefinement")
+        #output_mesh_2 = pymesh.boolean(mesh_D, mesh_E, operation="union", engine="corefinement")
+        # Use difference if the endo normals are pointing inside
+        output_mesh_2 = pymesh.boolean(mesh_E, mesh_D, operation="difference", engine="corefinement")
         pymesh.save_mesh(job.ID + "/bridges/" + str(var) + "_union_to_resample.obj", output_mesh_2, ascii=True)
         print("Union between earth and bridges in " + var)
 
